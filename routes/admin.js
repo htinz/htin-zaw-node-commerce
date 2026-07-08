f8590d8f1d636cf7f5e8becdea12292119e96902
Htin Z <htindev@gmail.com>
2026-07-08 03:44:29 +0100

    updated

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 // router.get("/add-product", (req, res, next) => {
 //   res.send(
 //     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>',
@@ -16,10 +18,10 @@ router.get("/add-product", (req, res, next) => {
 });
 
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  // console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
-
-
+exports.routes = router;
+exports.products = products;
